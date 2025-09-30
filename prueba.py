430c2520a14b43a888514b8ae7ea7a70200e0e0b
Fran Gómez <fgomrom726@g.educaand.es>
2025-09-30 13:49:20 +0200

    Primer commit

---
+++ prueba.py
@@ -0,0 +1,9 @@
+a = input("Primer número")
+b = input("Segundo número")
+if a > b:
+    print ("mayor: ", a)
+else:
+    if a < b:
+        print ("mayor: ", b)
+    else:
+        print ("iguales")

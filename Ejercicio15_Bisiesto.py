80a6ed6d7e74928028288d57304e5f0c6c306e92
fgomrom <113981624+fgomrom@users.noreply.github.com>
2025-10-01 11:07:56 +0200

    Update Ejercicio15_Bisiesto.py
    He añadido el anio en el mensaje

--- Ejercicio15_Bisiesto.py
+++ Ejercicio15_Bisiesto.py
@@ -1,5 +1,6 @@
 anio = int(input("Introduzca año: "))
 if (anio % 4 == 0 and anio % 100 != 0) o anio % 400 == 0:
-    print ("Bisiesto")
+    print ("Bisiesto", anio)
 else
-    print ("No bisiesto")
+    print ("No bisiesto", anioi)
+

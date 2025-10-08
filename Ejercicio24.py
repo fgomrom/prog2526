91c40ff3957694cdeb274810d5c26f80512f9d6b
Fran Gómez <fgomrom726@g.educaand.es>
2025-10-08 11:02:12 +0200

    Ejercicio 24

---
+++ Ejercicio24.py
@@ -0,0 +1,7 @@
+suma = 0
+while True :
+    numero = float(input("Introduce un número (0 para terminar): "))
+    if numero == 0:
+        break
+    suma += numero 
+print("La suma total es:", suma)

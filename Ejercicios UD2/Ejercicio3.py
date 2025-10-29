dc50c81b1b0d1e8ed7bb58c08abb591f13a96c9d
Fran Gómez <fgomrom726@g.educaand.es>
2025-10-29 13:01:29 +0100

    Ejercicio 4 y 5

--- Ejercicios UD2/Ejercicio3.py
+++ Ejercicios UD2/Ejercicio3.py
@@ -1,3 +1,6 @@
 a = [1,2,3,4]
+# Este for se sale del rango
+#for i in range(5):
+# Se corrige ajustando el rango al tamaño del array
 for i in range(len(a)):
     print(a[i])
---
+++ Ejercicios UD2/Ejercicio5.py
@@ -0,0 +1,4 @@
+a = [0]*5
+for i in range(5):
+    a[i] = float(input("Introduzca número decimal:"))
+print(a)

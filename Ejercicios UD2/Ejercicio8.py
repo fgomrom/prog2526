28df4912500b4fd98245c1c63397fea35f1d01cc
Fran Gómez <fgomrom726@g.educaand.es>
2025-11-03 14:07:25 +0100

    Operaciones básicas

---
+++ Ejercicios UD2/Ejercicio8.py
@@ -0,0 +1,10 @@
+# Ejemplo dividir array (Slice)
+a = [1,2,3,4,5,6,7,8]
+print(a)
+print(a[0:4:2]) # Cogería los dos primenos números impares
+
+# Ejercicio 8
+nombre = ['f','r','a','n',' ', 'g','o','m','e','z']
+print(nombre)
+print(nombre[0:4])
+print(nombre[5:len(nombre)])

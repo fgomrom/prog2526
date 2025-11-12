cd23fe81197e8b15aa120da61fc5c741b7fdb1e1
Fran Gómez <fgomrom726@g.educaand.es>
2025-11-12 10:56:12 +0100

    Ejercicio19 enunciado

---
+++ UD2/Ejercicios/Ejercicio19.py
@@ -0,0 +1,17 @@
+# Ejercicio 19: Comparacion de arrays
+lista_a = [10, 20, 30]
+lista_b = [10, 20, 30]
+lista_c = [30, 20, 10] # Mismos elementos, orden diferente
+lista_d = [10, 20]     # Longitud diferente
+
+print(f"A == B: {lista_a == lista_b}") # True
+print(f"A == C: {lista_a == lista_c}") # False (el orden importa)
+print(f"A == D: {lista_a == lista_d}") # False (la longitud importa)
+
+#TODO: Algoritmo que dice si dos arrays son iguales
+def iguales(lista1, lista2):
+    
+    
+    #TODO: retorna un valor boleano
+
+

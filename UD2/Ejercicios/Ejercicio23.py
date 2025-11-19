626c7b99175f8193b1a3b64d1447d79e771e0710
Fran Gómez <fgomrom726@g.educaand.es>
2025-11-19 10:56:32 +0100

    While

--- UD2/Ejercicios/Ejercicio23.py
+++ UD2/Ejercicios/Ejercicio23.py
@@ -1,9 +1,14 @@
 p = [[5.0,10.5],[8.2,15.0],[1.5,3.0]]
 suma = 0
 for i in range(len(p)):
-    for j in range(len(p[0])):
-        if j == 1:
-            suma += p[i][j]
+    suma += p[i][1]
+print(f"La suma es:{suma}")
+
+suma = 0
+i = 0
+while i < len(p):
+    suma = suma + p[i][1]
+    i = i + 1
 print(f"La suma es:{suma}")
 
         

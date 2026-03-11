636cc82b46533051c60b05b540429377da5cf537
Fran Gómez <fgomrom726@g.educaand.es>
2026-03-11 13:04:21 +0100

    equals

--- UD5/Ejercicios/src/prog/comunes/Persona.java
+++ UD5/Ejercicios/src/prog/comunes/Persona.java
@@ -4,106 +4,22 @@ import prog.ud4.ejercicios.ejercicio2.DiaDeLaSemana;
 
 public class Persona {
 	// Atributos
-	String nombre;
-	public String getNombre() {
-		return nombre;
-	}
+	public String nombre;
+	public int edad;
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-
-	public float getEstatura() {
-		return estatura;
-	}
-
-	public void setEstatura(float estatura) {
-		this.estatura = estatura;
-	}
-
-	public DiaDeLaSemana getDia() {
-		return dia;
-	}
-
-	public void setDia(DiaDeLaSemana dia) {
-		this.dia = dia;
-	}
-
-	public Sexo getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(Sexo sexo) {
-		this.sexo = sexo;
-	}
-
-	String apellido;
-	int edad;
-	float estatura;
-	DiaDeLaSemana dia;
-	Sexo sexo;
-
-	// Constructores
-	Persona(String nombre, String apellido, int edad, float estatura, Sexo sexo) {
-		this(nombre, apellido, edad, estatura);
-		this.sexo = sexo;
-	}
-
-	Persona(String nombre, String apellido, int edad, float estatura) {
-		this(nombre + " " + apellido, edad, estatura);
-		this.apellido = apellido;
-	}
-
-	Persona(String nombre, int edad, float estatura) {
+	public Persona(String nombre, int edad) {
 		this.nombre = nombre;
 		this.edad = edad;
-		this.estatura = estatura;
 	}
 
-	Persona(String nombre) {
-		this.nombre = nombre;
+	@Override
+	public String toString() {
+		return nombre + " " + edad;
 	}
 
-	Persona(int edad) {
-		this.edad = edad;
+	@Override
+	public boolean equals(Object obj) {
+		return this.nombre.equals(((Persona) obj).nombre) &&
+				this.edad == ((Persona) obj).edad;
 	}
-
-	public Persona() {
-		this.nombre = "anónino";
-	}
-
-	// Métodos
-	void saludar() {
-		System.out.println("Hola!");
-	}
-
-	void cumplirAnios() {
-		edad++;
-	}
-
-	void crecer(int incremento) {
-		estatura += incremento;
-	}
-
-	public enum Sexo {
-		MASCULINO, FEMENINO, NO_BINARIO
-	}
-
-	
 }
--- UD5/Ejercicios/src/prog/ud5/ejemplos/object/Main.java
+++ UD5/Ejercicios/src/prog/ud5/ejemplos/object/Main.java
@@ -5,10 +5,9 @@ import prog.comunes.Persona;
 public class Main {
 
 	public static void main(String[] args) {
-		Persona fran = new Persona();
-		fran.setNombre("Fran Gómez");
-		System.out.println(fran);
-		fran.toString();
+		Persona p1 = new Persona(null, 20);
+		Persona p2 = new Persona("David", 20);
+		System.out.println(p1.equals(p1));
 	}
 
 }

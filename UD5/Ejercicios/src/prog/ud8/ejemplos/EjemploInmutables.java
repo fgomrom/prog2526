16f32c38c32b7ab008a351a7d4537d87c002451c
Fran Gómez <frangoro@gmail.com>
2026-04-13 11:30:57 +0200

    Inmutables

---
+++ UD5/Ejercicios/src/prog/ud8/ejemplos/EjemploInmutables.java
@@ -0,0 +1,25 @@
+package prog.ud8.ejemplos;
+
+public class EjemploInmutables {
+
+	public static void main(String[] args) {
+		String s1 = "Hola";              // Va al Pool
+		String s2 = "Hola";              // Referencia al mismo objeto en el Pool
+		String s3 = new String("Hola");  // Se crea un objeto NUEVO en el Heap (fuera del Pool)
+
+		System.out.println(s1 == s2);    // true
+		System.out.println(s1 == s3);    // false
+		
+		
+		String texto = "Hola";
+        texto.concat(" mundo"); // Intentamos modificar el contenido
+
+        System.out.println(texto); // Imprime: Hola
+
+        // Guardamos el resultado en una nueva variable
+        String nuevoTexto = texto.concat(" mundo");
+
+        System.out.println(nuevoTexto); // Imprime: Hola mundo
+	}
+
+}
---
+++ UD5/Ejercicios/src/prog/ud8/ejercicios/Ejercicio4.java
@@ -0,0 +1,11 @@
+package prog.ud8.ejercicios;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}

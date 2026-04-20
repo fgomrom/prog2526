89763b2d7f45fa3eebb931090eb3148408ec337f
Fran Gómez <frangoro@gmail.com>
2026-04-20 10:13:32 +0200

    Comparable

--- UD5/Ejercicios/src/prog/comunes/Persona.java
+++ UD5/Ejercicios/src/prog/comunes/Persona.java
@@ -1,12 +1,11 @@
 package prog.comunes;
 
-import prog.ud4.ejercicios.ejercicio2.DiaDeLaSemana;
-
-public class Persona {
+public class Persona implements Comparable{
 	// Atributos
-	public String nombre;
-	public int edad;
-
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
 	public Persona(String nombre, int edad) {
 		this.nombre = nombre;
 		this.edad = edad;
@@ -19,17 +18,17 @@ public class Persona {
 	public Persona(String nombre) {
 		this.nombre = nombre;
 	}
-
-	public void setEdad(int edad) {
-		if (edad < 0) {
-			throw new EdadInvalidaException("Edad negativa");
-		}
+	
+	public Persona(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
 		this.edad = edad;
 	}
 
 	@Override
 	public String toString() {
-		return nombre;
+		return nombre + " " + apellido + " - " + edad;
 	}
 
 	@Override
@@ -48,5 +47,25 @@ public class Persona {
 
 	public int getEdad() {
 		return edad;
+	}
+	
+	public void setEdad(int edad) {
+		if (edad < 0) {
+			throw new EdadInvalidaException("Edad negativa");
+		}
+		this.edad = edad;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	@Override
+	public int compareTo (Object p) {
+		return this.getNombre().compareTo(((Persona)p).getNombre());
 	}
 }

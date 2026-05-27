7bb24fba3fa5f02be44be09f277f5db44fe850ea
Fran Gómez <fgomrom726@g.educaand.es>
2026-05-27 10:56:04 +0200

    Crear POJO

--- UD5/Ejercicios/src/prog/ud10/ejercicios/EventoMusical.java
+++ UD5/Ejercicios/src/prog/ud10/ejercicios/EventoMusical.java
@@ -1,20 +1,54 @@
 package prog.ud10.ejercicios;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Date;
-import javax.persistence.Entity;
 
-@Entity(name = "Evento")
-public class EventoMusical {
+public class EventoMusical implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
 	private String nombre;
 	private Date fecha;
 	private BigDecimal recaudacion;
+	
+	public EventoMusical() {
+		
+	}
+	
 	public EventoMusical(String nombre, Date fecha, BigDecimal recaudacion) {
 		super();
 		this.nombre = nombre;
 		this.fecha = fecha;
 		this.recaudacion = recaudacion;
 	}
-	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public BigDecimal getRecaudacion() {
+		return recaudacion;
+	}
+
+	public void setRecaudacion(BigDecimal recaudacion) {
+		this.recaudacion = recaudacion;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	
 }

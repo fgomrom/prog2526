53d61be9e98beb28b114ab071e3b154483522a86
Fran Gómez <fgomrom726@g.educaand.es>
2026-06-08 14:55:13 +0200

    Actualizo

--- UD5/Ejercicios/src/prog/ud11/ejemplos/evento/EjemploEvento.java
+++ UD5/Ejercicios/src/prog/ud11/ejemplos/evento/EjemploEvento.java
@@ -5,6 +5,8 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class EjemploEvento extends Frame{
 	
@@ -25,7 +27,52 @@ public class EjemploEvento extends Frame{
 				System.out.println("Botón pulsado!");
 			}
 		});
+		
+		WindowListener wl = new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
 
+		addWindowListener(wl);
 		setVisible(true);
 	}
 
